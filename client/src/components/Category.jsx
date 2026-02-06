d3319183a6f4e935d21db69c3378bd702d760d38
sandy424 <sandy424@naver.com>
2026-02-06 22:19:30 +0900

    [add] 선택 요소 색상 변경하기

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -3,12 +3,13 @@ import { useState } from 'react';
 import CategoryBtn from '../assets/icons/category.svg?react';
 import style from '../css/Home.module.css';
 
+const categories = ["전체", "빵", "케이크", "타르트", "구움과자", "기타"];
 
 function Category(){
 
     const [category, setCategory] = useState(false);
+    const [select, setSelect] = useState("전체");
     
-
     return(
         <div className={style.category}>
             <button onClick={() => setCategory(!category)}>
@@ -17,11 +18,15 @@ function Category(){
 
             {category && (
                 <ul className={style.categoryli}>
-                    <li>빵</li>
-                    <li>케이크</li>
-                    <li>타르트</li>
-                    <li>구움과자</li>
-                    <li>기타</li>
+                    {categories.map((cat) => (
+                        <li
+                            key={cat}
+                            onClick={() => setSelect(cat)}
+                            className={select === cat ? style.select : ""}>
+                            
+                            {cat}
+                        </li>
+                    ))}
                 </ul>
             )}
         </div>
