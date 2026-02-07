08ae69e9d0d1824b859f40385914deaa505d0617
sandy424 <sandy424@naver.com>
2026-02-07 16:16:45 +0900

    [add] 지도 마커 생성

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,53 +1,53 @@
 import { useEffect,useRef, useState } from "react";
 import style from '../css/Home.module.css';
+import data from '../data/db.json';
 
 import Bottombar from "../components/Bottombar";
 import Category from "../components/Category";
 
 function Home() {
   const [stores, setStores] = useState([]);
+
+  const mapDivRef = useRef(null);
   const mapRef = useRef(null);
   const markerRef = useRef([]);
 
-  const fetchData = async() => {
-    try {
-      const response = await fetch("/data/db.json");
-      const json = await response.json();
-      setStores(json);
-    } catch(error) {
-      console.log("데이터 불러오기 실패", error);
-    }
-  };
-
   useEffect(() => {
     // eslint-disable-next-line react-hooks/set-state-in-effect
-    fetchData();
+    setStores(data.stores);
   }, []);
 
   useEffect(() => {
     const { naver } = window;
-
-    if (!naver?.maps || !mapRef.current) return; // 네이버 스크립트 로드 전 보호 코드
+    if (!naver?.maps || !mapDivRef.current) return; // 네이버 스크립트 로드 전 보호 코드
 
     const location = new naver.maps.LatLng(35.1796, 129.0756);
-
-    mapRef.current = new naver.maps.Map(mapRef.current, {
+    mapRef.current = new naver.maps.Map(mapDivRef.current, {
       center: location,
       zoom: 15,
     });
   }, []); // 처음 렌더링 한 번만 실행
 
+  //마커 생성
   useEffect(() => {
-    if(!mapRef.current || stores.length === 0) return;
-    
-    markerRef.curr
-    stores.forEach
-  })
+    if(!mapRef.current) return;
+    // 기존 마커 제거
+    markerRef.current.forEach(marker => marker.setMap(null));
+    markerRef.current = [];
+
+    stores.forEach(store => {
+      const marker = new window.naver.maps.Marker({
+        position: new window.naver.maps.LatLng(store.lat, store.lng),
+        map: mapRef.current,
+      });
+      markerRef.current.push(marker);
+    });
+  }, [stores]);
   
   return (
     <div className={style.container}>
       <div
-        ref={mapRef}
+        ref={mapDivRef}
         style={{ width: "100%", height: "100%" }}/>
       <Category />
       <Bottombar />
