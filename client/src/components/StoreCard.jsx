816e093173b5b4a1333e76baf6ab57c53e6ae5de
sandy424 <sandy424@naver.com>
2026-03-12 18:32:31 +0900

    [add] rating 추가

--- client/src/components/StoreCard.jsx
+++ client/src/components/StoreCard.jsx
@@ -1,4 +1,5 @@
 import style from "../css/Storecard.module.css";
+import RateStar from "../assets/icons/RateStar.svg?react";
 
 function StoreCard({ store, onClose }) {
   if (!store) return null;
@@ -8,6 +9,10 @@ function StoreCard({ store, onClose }) {
       <div className={style.header}>
         <p>{store.category}</p>
         <h2>{store.name}</h2>
+        <div className={style.star}>
+          <RateStar width={28} height={28}/>
+          <span>{store.rating}</span>
+        </div>
       </div>
       <button className={style.closeBtn} onClick={onClose}>✕</button>
     </div>
