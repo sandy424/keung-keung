45e4cfa55fbc130df04934748969323d16d122bd
sandy424 <sandy424@naver.com>
2026-03-17 16:12:54 +0900

    [add] 상점 미니 카드에 progress 추가

--- client/src/components/StoreCard.jsx
+++ client/src/components/StoreCard.jsx
@@ -5,8 +5,9 @@ import SaveHeart from '../assets/icons/SaveHeart.svg?react';
 function StoreCard({ store, onClose }) {
   if (!store) return null;
 
+
   return (
-    <div className={style.card}>
+    <div className={style.card}> 
       {/* header */}
       <div className={style.header}>
         <p>{store.category}</p>
@@ -27,7 +28,28 @@ function StoreCard({ store, onClose }) {
       <div className={style.explain}>
         가게 설명
       </div>
-
+      
+      {/* 점수표 */}
+      <div className={style.progressWrap}>
+        <p>맛</p>
+        <progress value={(store.rating / 5) * 100} max={100} />
+        <div className={style.progressLabel}>{Math.round((store.rating / 5) * 100)}%</div>
+      </div>
+      <div className={style.progressWrap}>
+        <p>분위기</p>
+        <progress value={(store.rating / 5) * 100} max={100} />
+        <div className={style.progressLabel}>{Math.round((store.rating / 5) * 100)}%</div>
+      </div>
+      <div className={style.progressWrap}>
+        <p>친절</p>
+        <progress value={(store.rating / 5) * 100} max={100} />
+        <div className={style.progressLabel}>{Math.round((store.rating / 5) * 100)}%</div>
+      </div>
+      <div className={style.progressWrap}>
+        <p>가격</p>
+        <progress value={(store.rating / 5) * 100} max={100} />
+        <div className={style.progressLabel}>{Math.round((store.rating / 5) * 100)}%</div>
+      </div>
       <div className={style.detailpage}>
         <button>상세페이지</button>
       </div>
