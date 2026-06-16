f9005dea718fe57d3a5d48c3a8e183dca6f01501
DESKTOP-Q5CFOMV\user <sandy424@naver.com>
2026-06-16 18:54:26 +0900

    ⚡️ Delete: Firebase Analytics를 차단 삭제

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -14,7 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
